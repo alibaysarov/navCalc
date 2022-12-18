14cc1ed3b9cdddb0179bc4d789c036a8db0ff9ad
alibaysarov <alibajsarov353@gmail.com>
2022-12-18 23:33:26 +0300

    Добавлен функционал путевой скорости

--- nav-calc/src/App.jsx
+++ nav-calc/src/App.jsx
@@ -1,41 +1,44 @@
 import React from "react";
 import Number from './mathLogic.js';
-import {findMeDriftAngle} from './aeroNavLogic.js'
+import {groundSpeed} from './aeroNavLogic.js'
 function App() {
   const [airspeedInput,setAirSpeedInput]=React.useState('');
   const airspeedInputHandler=(e)=>{
-    e.target.value=Number(e.target.value.replace(/\D/g,''))
+    e.target.value=e.target.value.replace(/(^0{1,1}|[A-Za-zА-Яа-я])/g,'')
     setAirSpeedInput(e.target.value);
     
   }
   const [windSpeedInput,setWindSpeedInput]=React.useState('');
   const windSpeedInputHandler=(e)=>{
-    e.target.value=Number(e.target.value.replace(/\D/g,''))
+    e.target.value=e.target.value.replace(/(^0{1,1}|[A-Za-zА-Яа-я])/g,'')
     setWindSpeedInput(e.target.value);
     
   }
   const [course,setCourse]=React.useState('');
   const courseInputHandler=(e)=>{
-    e.target.value=Number(e.target.value.replace(/\D/g,''))
+    e.target.value=e.target.value.replace(/(^0{1,1}|[A-Za-zА-Яа-я])/g,'')
     setCourse(e.target.value);
     
   }
   const [windDirection,setWindDirection]=React.useState('');
   const windDirectionHandler=(e)=>{
-    e.target.value=Number(e.target.value.replace(/\D/g,''))
+    e.target.value=e.target.value.replace(/(^0{1,1}|[A-Za-zА-Яа-я])/g,'')
     setWindDirection(e.target.value);
     
   }
-  
-  let res=findMeDriftAngle(airspeedInput,windSpeedInput,course,windDirection)
- 
+  const airSpd=Number(airspeedInput);
+  const windSpd=Number(windSpeedInput);
+  const magnetCourse=Number(course);
+  const windDir=Number(windDirection);
+  const res=groundSpeed(airSpd,windSpd,magnetCourse,windDir)
+ console.log(res)
   return (
    <div>
     <input type="text" value={airspeedInput} placeholder="Воздушная скорость" onChange={airspeedInputHandler} />
     <input type="text" value={windSpeedInput} placeholder="Скорость ветра" onChange={windSpeedInputHandler} />
-    <input type="text" value={course} placeholder="Путевой угол" onChange={courseInputHandler} onBlur={()=>setCourse('')} />
+    <input type="text" value={course} placeholder="Путевой угол" onChange={courseInputHandler}  />
     <input type="text" value={windDirection} placeholder="Направление ветра" onChange={windDirectionHandler} />
-    <h3>{'Угол сноса равен:'+res}</h3>
+    <h3>{`Путевая скорость равна ${res}`}</h3>
    </div>
   );
 }
--- nav-calc/src/aeroNavLogic.js
+++ nav-calc/src/aeroNavLogic.js
@@ -1,7 +1,20 @@
 import Number from "./mathLogic";
-export const findMeDriftAngle=(airSpd,windSpd,magnHdg,windDir)=>{
-    const navWind=windDir>180?windDir-180:windDir+180;
-    const courseWindAgle=navWind-magnHdg;
-    let driftAngle=(courseWindAgle*windSpd/airSpd)*courseWindAgle.toSin();
-     return driftAngle
-  }
+
+//Угол сноса
+
+export const groundSpeed=(airSpd,windSpd,magnHdg,windDir)=>{
+    const findMeDriftAngle=(airSpd,windSpd,magnHdg,windDir)=>{
+        const navWind=windDir>180?windDir-180:windDir+180;
+        const courseWindAngle=navWind-magnHdg;
+        let driftAngle=(courseWindAngle*windSpd/airSpd)*courseWindAngle.toSin();
+         return isNaN(driftAngle)?'---':{driftAngleCos:driftAngle.toCos(),courseWindAgleCos:courseWindAngle.toCos()}
+    }
+
+    const res=findMeDriftAngle(airSpd,windSpd,magnHdg,windDir);
+    console.log(res)
+    const {driftAngleCos,courseWindAgleCos}=res;
+    return isNaN((airSpd*driftAngleCos+windSpd*courseWindAgleCos).toFixed(1))
+    ?'---'
+    :(airSpd*driftAngleCos+windSpd*courseWindAgleCos).toFixed(1)
+    
+}
