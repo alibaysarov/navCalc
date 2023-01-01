ce821a6e6a222d48490ee637873c08ac819e7cf6
alibaysarov <alibajsarov353@gmail.com>
2023-01-01 06:41:38 +0300

    Добавил клики по карте

--- nav-calc/src/components/Map/index.jsx
+++ nav-calc/src/components/Map/index.jsx
@@ -11,7 +11,8 @@ import MultiPoint from 'ol/geom/MultiPoint.js';
 import {transform} from 'ol/proj';
 import Feature from 'ol/Feature';
 import Polygon from 'ol/geom/Polygon.js';
-
+import {Circle, Fill, Stroke, Style} from 'ol/style.js';
+import LineString from 'ol/geom/LineString.js';
 
 const MapWrapper = () => {
   const options = {
@@ -42,25 +43,94 @@ const MapWrapper = () => {
   const [ map, setMap ] = useState()
   const [ featuresLayer, setFeaturesLayer ] = useState()
   const [ selectedCoord , setSelectedCoord ] = useState()
+  const [coords,setCoords]=useState([])
   const mapRef=useRef(null)
   const mapStateRef=useRef(null)
   mapStateRef.current=map
-  const handleMapClick = (event) => {
-    
-    // get clicked coordinate using mapRef to access current React state inside OpenLayers callback
-    //  https://stackoverflow.com/a/60643670
-    const clickedCoord = mapStateRef.current.getCoordinateFromPixel(event.pixel);
-    
-    // transform coord to EPSG 4326 standard Lat Long
-    const transormedCoord = transform(clickedCoord, 'EPSG:3857', 'EPSG:4326')
-    console.log(transormedCoord)
-    // set React state
-    setSelectedCoord( transormedCoord )
-    
-  }
+ 
   useEffect(()=>{
+    const handleMapClick = (event) => {
+    
+      // get clicked coordinate using mapRef to access current React state inside OpenLayers callback
+      //  https://stackoverflow.com/a/60643670
+      
+      const clickedCoord = mapStateRef.current.getCoordinateFromPixel(event.pixel);
+      const transformedCoord = transform(clickedCoord, 'EPSG:3857', 'EPSG:4326')
+      coords.push(transformedCoord)
+      console.log(coords)
+      
+      const styles=new Style({
+        image:new Circle({
+          radius:6,
+          fill:new Fill({color:'red'}),
+          stroke:new Stroke({
+            color:[0,0,0],
+            width:5
+          })
+        }),
+        stroke:new Stroke({
+          color:'red',
+          width:5,
+        })
+      })
+      // console.log(styles.stroke)
+      const stroke = new Style({
+        fill:new Fill({
+          color:'#000'
+        }),
+        stroke:new Stroke({
+          color: 'magenta',
+          width: 4,
+        })
+      })
+      
+      // const styleFunction = function (feature) {
+      //   const geometry = feature.getGeometry();
+      //   const styles = [
+      //     // linestring
+      //     new Style({
+      //       stroke: new Stroke({
+      //         color: [0,0,0],
+      //         width: 2,
+      //       }),
+      //     }),
+      //   ];
+      // }
+      
+      let lines = new LineString(coords).transform('EPSG:4326', initialMap.getView().getProjection());
+      let secondLayer=new VectorLayer({
+        source:new VectorSource({
+          features: [
+            new Feature({
+              geometry: lines,
+              name: "Line",
+            }),
+          ],
+        }),
+        style:stroke
+      })
+      // secondLayer.setStroke(stroke)
+      initialMap.addLayer(secondLayer)
+
+
+      // const featureToAdd=new Feature({
+      //   geometry:new Point(event.coordinate)
+      // })
+      
+      // featureToAdd.setStyle(styles)
+      // // initialFeaturesLayer.source.addFeature(featureToAdd)
+      // console.log(featureToAdd)
+      // transform coord to EPSG 4326 standard Lat Long
+      
+      console.log(transformedCoord)
+      // set React state
+      setSelectedCoord( transformedCoord )
+      
+    }
+    
     const initialFeaturesLayer=new VectorLayer({
-      source:new VectorSource()
+      source:new VectorSource(),
+      
     })
     const initialMap=new Map({
       target:mapRef.current,
@@ -80,38 +150,14 @@ const MapWrapper = () => {
       }),
       controls:[]
     })
-
+    
     setMap(initialMap)
     setFeaturesLayer(initialFeaturesLayer)
     initialMap.on('click',handleMapClick)
   },[])
-  let feature1=new Feature({
-    geometry: new Polygon(),
-    labelPoint: new Point([-11.0000,46.00000],"Point"),
-    name: 'My Polygon'
-  })
-  const features=[feature1]
-  useEffect(() => {
   
-    if (features.length) { // may be empty on first render
-
-      if(featuresLayer){
-        // set features to map
-      featuresLayer.setSource(
-        new VectorSource({
-          features: features // make sure features is an array
-        })
-      )
-
-      // fit map to feature extent (with 100px of padding)
-      map.getView().fit(featuresLayer.getSource().getExtent(), {
-        padding: [100,100,100,100]
-      })
-      }
-
-    }
-
-  },[features])
+  
+  
   
   return (
     <div style={{height:400+'px',width:100+'%'}} className={cl.map} ref={mapRef}>
