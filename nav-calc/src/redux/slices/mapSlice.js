3131f50e9ca5cac1f704a856d3626a88ea0fc2c8
alibaysarov <alibajsarov353@gmail.com>
2023-05-06 00:17:03 +0300

    changes

--- nav-calc/src/redux/slices/mapSlice.js
+++ nav-calc/src/redux/slices/mapSlice.js
@@ -1,59 +1,63 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
-import { useDispatch } from 'react-redux';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
-export const getMeZones=createAsyncThunk('ui/getMeZones',async()=>{
-  const res=await fetch('http://localhost:5000/api/zones')
-  if(res.ok){
-    const json=await res.json();
+export const getMeZones = createAsyncThunk("ui/getMeZones", async () => {
+  const res = await fetch(`${layersUrl}api/zones`);
+  if (res.ok) {
+    const json = await res.json();
     return json.data;
   }
   console.log(res);
-  console.log('23432424234');
+  console.log("23432424234");
 });
-export const getMeAirports=createAsyncThunk('ui/getMeAirports',async(_,{getState})=>{
-const {viewPortConstr}=getState().map;
-const [lng1,lat1,lng2,lat2]=viewPortConstr;
-const url='http://localhost:5000/api/airports'+`?lng1=${lng1}&lat1=${lat1}&lng2=${lng2}&lat2=${lat2}`
-const res=await fetch(url)
-  if(res.ok){
-    const json=await res.json();
-    console.log(json);
-    return json.data
-  }
-});
-export const mapSlice=createSlice({
-  name:'map',
-  initialState:{
-    isLineAdded:false,
-    zones:[],
-    airports:[],
-    viewPortConstr:[]
-  },
-  reducers:{
-    viewPortHandler:(state,action)=>{
-      state.viewPortConstr=action.payload
-    },
-    drawHandler:(state)=>{
-      state.isLineAdded=true
-    },
-    removeAirports:(state)=>{
-      state.airports=[]
-    },
-    removeZones:(state)=>{
-      state.zones=[]
+export const getMeAirports = createAsyncThunk(
+  "ui/getMeAirports",
+  async (_, { getState }) => {
+    const { viewPortConstr } = getState().map;
+    const [lng1, lat1, lng2, lat2] = viewPortConstr;
+    const url = `${layersUrl}api/airports?lng1=${lng1}&lat1=${lat1}&lng2=${lng2}&lat2=${lat2}`;
+    const res = await fetch(url);
+    if (res.ok) {
+      const json = await res.json();
+      console.log(json);
+      return json.data;
     }
+  }
+);
+export const mapSlice = createSlice({
+  name: "map",
+  initialState: {
+    isLineAdded: false,
+    zones: [],
+    airports: [],
+    viewPortConstr: [],
   },
-  extraReducers:{
-    [getMeZones.fulfilled]:(state,action)=>{
-      state.zones=action.payload;
+  reducers: {
+    viewPortHandler: (state, action) => {
+      state.viewPortConstr = action.payload;
+    },
+    drawHandler: (state) => {
+      state.isLineAdded = true;
+    },
+    removeAirports: (state) => {
+      state.airports = [];
+    },
+    removeZones: (state) => {
+      state.zones = [];
+    },
+  },
+  extraReducers: {
+    [getMeZones.fulfilled]: (state, action) => {
+      state.zones = action.payload;
       console.log(state.zones);
-      console.log('zones added');
+      console.log("zones added");
     },
-    [getMeAirports.fulfilled]:(state,action)=>{
-      state.airports=action.payload
-    }
-  }
-})
+    [getMeAirports.fulfilled]: (state, action) => {
+      state.airports = action.payload;
+    },
+  },
+});
 
-export const {drawHandler,removeZones,removeAirports,viewPortHandler}=mapSlice.actions;
+export const { drawHandler, removeZones, removeAirports, viewPortHandler } =
+  mapSlice.actions;
 export default mapSlice.reducer;
